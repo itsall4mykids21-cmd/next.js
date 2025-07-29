34897d9dbcd76b88a6ba28eb6b27415c14760e03
Jiachi Liu <inbox@huozhi.im>
2025-07-29 18:11:53 +0200

    fix layout assertion

--- packages/next/src/build/webpack/loaders/next-app-loader/index.ts
+++ packages/next/src/build/webpack/loaders/next-app-loader/index.ts
@@ -781,10 +781,7 @@ const nextAppLoader: AppLoader = async function nextAppLoader() {
 
   // Doing the layout assertion to ensure layout is defined.
   // Skip assertion for app global-error and global-not-found route since it is page.js already contains html/body.
-  if (
-    !treeCodeResult.rootLayout &&
-    (!isGlobalNotFoundPath || isAppErrorRoute)
-  ) {
+  if (!treeCodeResult.rootLayout && !isGlobalNotFoundPath && !isAppErrorRoute) {
     if (!isDev) {
       // If we're building and missing a root layout, exit the build
       Log.error(
