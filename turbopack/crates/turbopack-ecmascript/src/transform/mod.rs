e434b983643bf1d7bdde6d9edabdc76842e67130
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-05 10:16:21 +0200

    Bump to swc v38

--- turbopack/crates/turbopack-ecmascript/src/transform/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/transform/mod.rs
@@ -147,7 +147,6 @@ impl EcmascriptInputTransform {
                     program,
                     helpers,
                     inline_globals(
-                        unresolved_mark,
                         Default::default(),
                         Default::default(),
                         Default::default(),
